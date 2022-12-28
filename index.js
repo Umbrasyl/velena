b7b3af8974820963f525d00d4d1e81bf88a6c1be
Mcorduk <corduk.5tp@gmail.com>
2022-12-28 13:23:06 +0300

    add pages

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 "use strict";
 
-const menuButton = document.querySelector(".menu-button");
-const menu = document.querySelector(".menu");
-
-navButton.addEventListener("click", () => {
-  menu.classList.toggle(".visible");
-})
